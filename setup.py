ac2a8bcd2301ea8824f0938f6084a23bb0ca7516
Brent Woodruff <brent@fprimex.com>
2023-06-30 17:25:14 -0400

    v1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="asplode",
-    version="0.1.0",
+    version="1.0.0",
     description="Recursively decompress archives",
     long_description="Recursively decompress archives",
     classifiers=[
